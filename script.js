2031a21560b2ecc6cfb04bff33d3fd13a7b9807e
v.noemi1997@gmail.com <v.noemi1997@gmail.com>
2024-04-19 13:58:39 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const menuIcon = document.querySelector(".menu-icon");
 const container = document.querySelector(".container");
+const navigation = document.querySelector(".navigation");
+const navItems = document.querySelector(".nav-items");
 
 menuIcon.addEventListener("click", () => {
-  container.classList.toggle("change");
+  if (
+    !container.classList.contains("close") &&
+    !container.classList.contains("open")
+  ) {
+    container.classList.add("open");
+  } else if (container.classList.contains("close")) {
+    container.classList.replace("close", "open");
+  } else if (container.classList.contains("open")) {
+    container.classList.replace("open", "close");
+  }
+});
+
+container.addEventListener("click", (event) => {
+  if (
+    /* !container.classList.contains("close") && */
+    container.classList.contains("open") &&
+    !navigation.contains(event.target) &&
+    !menuIcon.contains(event.target)
+  ) {
+    container.classList.replace("open", "close");
+  }
+});
+
+navItems.addEventListener("click", () => {
+  container.classList.replace("open", "close");
 });
